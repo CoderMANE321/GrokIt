2d8d4c8b9fc3a03d1c4cb872a76f933f3b0b36a2
CODERMANE321 <deontamwilliams@gmail.com>
2025-04-03 20:19:08 -0400

    DEQ

--- base.py
+++ base.py
@@ -8,15 +8,4 @@ class :
         
 
 
-"""
-Space Complexity
-- O(n): Each appended value creates a new Node object. In the worst case, all nodes are stored at once.
-- O(1): Overhead for the dummy head and tail nodes.
 
-Time Complexity
-- append: O(1) – Constant time to insert at the tail.
-- appendleft: O(1) – Constant time to insert at the head.
-- pop: O(1) – Constant time to remove from the tail.
-- popleft: O(1) – Constant time to remove from the head.
-- isEmpty: O(1) – Just checks pointers.
-"""
--- source/structs/DoubleEndedQueue.py
+++ source/structs/DoubleEndedQueue.py
@@ -57,3 +57,17 @@ class Deque:
         next_node.prev = self.head
 
         return value
+
+
+"""
+Space Complexity
+- O(n): Each appended value creates a new Node object. In the worst case, all nodes are stored at once.
+- O(1): Overhead for the dummy head and tail nodes.
+
+Time Complexity
+- append: O(1) – Constant time to insert at the tail.
+- appendleft: O(1) – Constant time to insert at the head.
+- pop: O(1) – Constant time to remove from the tail.
+- popleft: O(1) – Constant time to remove from the head.
+- isEmpty: O(1) – Just checks pointers.
+"""
